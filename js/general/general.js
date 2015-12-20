37959349fa015095b9d67b35c4394e0dd4e4bfd0
Rob Everhardt <rob@everhardt.nl>
2015-12-20 21:39:12 +0100

    - Change CNAME to settlepad.com (full domain) - JS to api.settlepad.com (on AWS)

--- js/general/general.js
+++ js/general/general.js
@@ -26,8 +26,8 @@ jQuery.ajaxWrapper = function(resource, type, secure, data, notification, ajaxOp
 		$.ajax($.extend({
 			type: type,
 			contentType: contenttype,
-			url: "http://127.0.0.1/"+resource, //local
-			//url: "https://api.settlepad.com/"+resource, //web
+			//url: "http://127.0.0.1/"+resource, //local
+			url: "https://api.settlepad.com/"+resource, //web
 			dataType: "json",
 			data: data,
 			beforeSend: function(jqXHR, settings){
